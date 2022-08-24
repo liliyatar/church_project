2d2722b74e8fcc74d59a3d36d6c24110f468efe8
Liliia Kobzeva <liliia.taran@dev.pro>
2022-08-24 19:24:45 +0700

    Update PORT

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 8081;
+// const port = process.env.PORT || 8081;
+const port = 8080;
 
 app.listen(port, () => console.log(`Server has been started on ${port}`));
