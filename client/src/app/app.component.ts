7eecf5f095fce13ebd1f45e8a7a44c4f33027f02
Liliia Kobzeva <liliia.taran@dev.pro>
2023-07-06 15:00:17 +0700

    fix sidenav

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(() => {
-        window.history.forward();
-      })
-  }
+export class AppComponent {
+  constructor() {}
 }
--- client/src/app/shared/components/navbar/navbar.component.ts
+++ client/src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { ViewportScroller } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild, OnInit, HostListener } from '@angular/core';
 import { MaterialInstance, MaterialService } from '../../classes/material.service';
 
 @Component({
@@ -8,6 +8,18 @@ import { MaterialInstance, MaterialService } from '../../classes/material.servic
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements AfterViewInit, OnDestroy, OnInit {
+  @HostListener('window:swipe', ['$event'])
+  onSwipe(event: TouchEvent) {
+    const touch = event.touches[0];
+    const touchX = touch.clientX;
+    
+    const windowWidth = window.innerWidth;
+    const swipeThreshold = windowWidth * 0.2; // Пороговое значение свайпа, указывающее, насколько далеко нужно просвайпить для открытия меню
+
+    if (touchX < swipeThreshold) {
+      event.preventDefault();
+    }
+  }
   @ViewChild('sidenav') sidenavRef: ElementRef;
   public sidenav: MaterialInstance; 
   @ViewChild('dropdown') dropdownRef: ElementRef;
@@ -26,6 +38,8 @@ export class NavbarComponent implements AfterViewInit, OnDestroy, OnInit {
   
   ngAfterViewInit(): void {
     this.sidenav = MaterialService.initSidenav(this.sidenavRef);
+    console.log(this.sidenav)
+
     this.dropdown = MaterialService.initDropDown(this.dropdownRef);
     this.dropdown2 = MaterialService.initDropDown(this.dropdown2Ref);
     this.dropdown3 = MaterialService.initDropDown(this.dropdown3Ref);
