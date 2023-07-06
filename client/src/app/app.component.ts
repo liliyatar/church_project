30c39e5ba2b38a5d621f26477802148bb0703fc2
Liliia Kobzeva <liliia.taran@dev.pro>
2023-07-06 14:28:32 +0700

    fix opening sidenav

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.history.forward();
+      })
+  }
 }
